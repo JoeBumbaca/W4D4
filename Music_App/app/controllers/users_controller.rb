cbd74bdde49fd207e4229dee998a13212bb92b20
joe bumbaca <joebumbaca@gmail.com>
2019-06-06 11:08:59 -0700

    Add methods to controllers, Views for User and Session

--- Music_App/app/controllers/users_controller.rb
+++ Music_App/app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+
+  def create
+    @user = User.new(user_params)
+    if user.save!
+      login!(@user)
+      redirect_to user_url(@user)
+    else
+      redirect_to new_user_url
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
 end
--- Music_App/app/models/user.rb
+++ Music_App/app/models/user.rb
@@ -13,23 +13,24 @@
 class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :password_digest, :session_token, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true}
+
+  attr_reader :password
 
   after_initialize :ensure_session_token
 
   def self.find_by_credentials(email, password)
-    @user = User.find_by(params[:user][:email], params[:user][:password])
-    if @user
-      redirect_to user_url(@user)
-    else
-      render :new
-    end
+    user = User.find_by(email: email)
+    
+    return nil if user.nil?
+    user.is_pword?(password) ? user : nil
   end
 
   def self.generate_session_token
     self.session_token = SecureRandom.urlsafe_base64
   end
 
-  def reset_session_token
+  def reset_session_token!
     self.session_token = SecureRandom.urlsafe_base64
     self.save!
     self.session_token
